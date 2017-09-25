26baac78e112ceba80c77d765e083afab7a67a06
Derek Vawdrey <flyingpiechicken@gmail.com>
2017-09-25 12:14:18 -0600

    Created the string assignment

---
+++ String.playground/Contents.swift
@@ -0,0 +1,26 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var changeString : String = "I am a "
+changeString += " dog"
+
+let math = 5
+let mathMessage = "\(math) is a number... \(math * 2)"
+
+let countCharacter = "numameniao"
+
+print("this variable has \(countCharacter.characters.count) characters")
+
+let compareString1 = "hi"
+let compareString2 = "HI"
+
+if compareString1 == compareString2 {
+    
+    print ("They are equal")
+    
+}else{
+    
+    print ("They are not equal")
+    
+}
---
+++ String.swift
@@ -0,0 +1,9 @@
+//
+//  String.swift
+//  
+//
+//  Created by Vawdrey, Derek on 9/25/17.
+//
+//
+
+import Foundation
