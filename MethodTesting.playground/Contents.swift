675e1c42e2a04fc2d3b83229528d4b4df32efbf4
Derek Vawdrey <flyingpiechicken@gmail.com>
2017-09-27 12:04:29 -0600

    Added more methods to the playground.

--- MethodTesting.playground/Contents.swift
+++ MethodTesting.playground/Contents.swift
@@ -22,3 +22,25 @@ public func aBitLessSimple(name:String) -> Void
 aBitLessSimple(name: "Hi")
 let words = "Slimy Tuna"
 aBitLessSimple(name: words)
+
+public func namingExample(outerName innerName : String) -> Void
+{
+    print("The inner name is: \(innerName)")
+}
+
+namingExample(outerName: "DingDongChingChongBingBong")
+
+public func counting() -> Int
+{
+    return (7 * 77758)
+}
+
+print("I am not \(counting()) years old")
+
+if (counting() < 10)
+{
+    print("Math works in swift too")
+}else
+{
+    print("not very likely right now")
+}
