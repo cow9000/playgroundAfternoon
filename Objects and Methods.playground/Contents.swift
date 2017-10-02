ad00ea22940653d179fcbbc9bba99a4383cc3eb7
Derek Vawdrey <flyingpiechicken@gmail.com>
2017-10-02 12:48:55 -0600

    Added the OtherClass to this playground.

--- Objects and Methods.playground/Contents.swift
+++ Objects and Methods.playground/Contents.swift
@@ -9,4 +9,52 @@ public class SimpleClass{
     public init(){
         name = "My name is a222"
     }
+    
+    
+    //Getters
+    
+    public func returnName() -> String
+    {
+        return name
+    }
+    
+    //Setters
+    public func setName(name setName : String) -> Void
+    {
+        name = setName
+    }
+    
 }
+
+var sample = SimpleClass()
+print(sample.returnName())
+
+public class OtherClass{
+    private var favoriteNumber : Int
+    private var favoriteWord : String
+    
+    public init(){
+        favoriteNumber = Int()
+        favoriteWord = String()
+    }
+    
+    public init(favoriteNumber : Int, favoriteWord : String){
+        self.favoriteNumber = favoriteNumber
+        self.favoriteWord = favoriteWord
+    }
+    
+    public func changeInternalState() -> Void{
+        self.favoriteNumber *= 1234
+        self.favoriteWord += " and more and more"
+    }
+    
+}
+
+var secondClass = OtherClass()
+var thirdSample = OtherClass(favoriteNumber : 911, favoriteWord : "Words")
+
+
+secondClass.changeInternalState()
+thirdSample.changeInternalState()
+
+
